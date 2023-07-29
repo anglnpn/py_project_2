0c1328346c83f6c81f8a67eb59c471d76f249c24
angelina <angln.pnn@gmail.com>
2023-07-29 13:07:49 -0300

    word upper

--- str_func.py
+++ str_func.py
@@ -1,3 +1,12 @@
 def add(word):
+    """
+    Возвращает в верхнем регистре
+    """
     return word.title()
+
+
+def add_two_up(word):
+    """Получает слова.Возвращает слова
+    с буквами в верхнем регистре"""
+    return word.upper()
 
